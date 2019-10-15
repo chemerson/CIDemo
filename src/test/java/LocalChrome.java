898caae6955f00ffdf6ad5bfe5ad167085301d20
Chris Emerson <cme210@yahoo.com>
2019-10-14 22:26:03 -0400

    Add batch id for CI

--- src/test/java/LocalChrome.java
+++ src/test/java/LocalChrome.java
@@ -73,6 +73,9 @@ public class LocalChrome {
 
         BatchInfo batchInfo = new BatchInfo(BATCH_NAME);
         if(BATCH_ID!=null) batchInfo.setId(BATCH_ID);
+        try {
+            batchInfo.setId(System.getenv("APPLITOOLS_BATCH_ID"));
+        } catch (Exception e) { }
         eyes.setBatch(batchInfo);
 
         driver = utils.drivers.getLocalChrome(threadId);
