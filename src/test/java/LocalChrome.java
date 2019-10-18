82ca8146254180c4c0b623abaa16ee9f33cb0e49
Chris Emerson <cme210@yahoo.com>
2019-10-18 10:20:43 -0400

    Update LocalChrome.java

--- src/test/java/LocalChrome.java
+++ src/test/java/LocalChrome.java
@@ -83,7 +83,7 @@ public class LocalChrome {
 
 
         //Allows for filtering dashboard view
-        eyes.addProperty("SANDBOX", "Custom var");
+        eyes.addProperty("SANDBOX", "Custom var1");
 
         System.out.println("START THREAD ID - " + Thread.currentThread().getId() + " " + browserName + " " + browserVersion);
         System.out.println("baseBeforeClass took " + (System.currentTimeMillis() - before) + "ms");
